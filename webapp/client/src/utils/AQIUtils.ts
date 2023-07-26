10ba28555b6fa1573deb2f3b6c455190c684a501
Gregory Melsby <greg@gregexp.com>
2023-07-26 18:17:54 -0400

    created conversion from pm 2.5 to aqi

--- webapp/client/src/utils/AQIUtils.ts
+++ webapp/client/src/utils/AQIUtils.ts
@@ -9,13 +9,34 @@ export enum aqiCategory {
   Hazardous = 'Hazardous'
 }
 
+// map from AQI to air quality category
 const aqiBreakpoints = new Map([
   [0, aqiCategory.Good],
   [51, aqiCategory.Moderate],
   [101, aqiCategory.UnhealthyForSensitiveGroups],
   [151, aqiCategory.Unhealthy],
   [201, aqiCategory.VeryUnhealthy],
-  [301, aqiCategory.Hazardous]
+  [301, aqiCategory.Hazardous],
+  [401, aqiCategory.Hazardous],
+  [500, aqiCategory.Hazardous]
+]);
+
+// map from PM 2.5 breakpoints to AQI breakpoints
+const pm25Breakpoints = new Map ([
+  [0, 0],
+  [12.0, 50],
+  [12.1, 51],
+  [35.4, 100],
+  [35.5, 101],
+  [55.4, 150],
+  [55.5, 151],
+  [150.4, 200],
+  [150.5, 201],
+  [250.4, 300],
+  [250.5, 301],
+  [350.4, 400],
+  [350.5, 401],
+  [500.4, 500]
 ]);
 
 // takes in an numeric value, outputs the corresponding aqiCategory
@@ -24,4 +45,35 @@ export const categorizeAqi = (aqi: number) => {
   const categoryKey = Math.max(...([...aqiBreakpoints.keys()]
     .filter(b => b <= aqi)));
   return aqiBreakpoints.get(categoryKey);
+};
+
+export const convertPm25ToAqi = (pm25: number) => {
+  // truncate to one decimal place
+  pm25 = Math.trunc(pm25 * 10) / 10;
+
+  // validate input
+  if (pm25 < 0) throw new RangeError('PM 2.5 cannot be less than 0');
+  // concentration breakpoint <= pm25
+  const bpLow = Math.max(...([...pm25Breakpoints.keys()]
+    .filter(b => b <= pm25)));
+  // concentration breakpoint > pm25
+  const bpHigh = Math.min(...([...pm25Breakpoints.keys()]
+    .filter(b => b > pm25)));
+
+  console.log(bpHigh);
+  // case where there were no higher breakpoints, so we are off the high end of the scale
+  if (bpHigh === 0 || bpHigh === Infinity) return 500;
+
+  // get the corresponding values on the AQI breakpoint scale
+  const iLow = pm25Breakpoints.get(bpLow);
+  const iHigh = pm25Breakpoints.get(bpHigh);
+
+  if (iLow === undefined || iHigh === undefined) {
+    throw Error('Map key did not exist');
+  }
+
+  console.log(`${JSON.stringify({iLow, iHigh, bpLow, bpHigh})}`);
+
+  const unroundedAqi = ((iHigh - iLow) / (bpHigh - bpLow)) * (pm25 - bpLow) + iLow;
+  return Math.round(unroundedAqi);
 };
--- webapp/client/tests/aqi.test.ts
+++ webapp/client/tests/aqi.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it, assert } from 'vitest';
-import { categorizeAqi, aqiCategory } from '../src/utils/AQIUtils';
+import { categorizeAqi, aqiCategory, convertPm25ToAqi } from '../src/utils/AQIUtils';
 
 
 describe('categorizeAqi tests', () => {
@@ -49,6 +49,79 @@ describe('categorizeAqi tests', () => {
   it('hazardous lower bound', () => {
     assert.equal(categorizeAqi(301), aqiCategory.Hazardous);
   });
+});
+
+describe('convertPm25ToAqi tests', () => {
+  it('invalid aqi', () => {
+    expect(() => convertPm25ToAqi(-5)).toThrowError(/PM 2.5 cannot be less than 0/);
+  });
+
+  it('pm25 of 0', () => {
+    assert.equal(convertPm25ToAqi(0), 0);
+  });
+
+  it('pm25 of 12', () => {
+    assert.equal(convertPm25ToAqi(12), 50);
+  });
+
+  it('pm25 of 12.1', () => {
+    assert.equal(convertPm25ToAqi(12.1), 51);
+  });
+
+  it('pm25 of 35.4', () => {
+    assert.equal(convertPm25ToAqi(35.4), 100);
+  });
+
+
+  it('pm25 of 35.5', () => {
+    assert.equal(convertPm25ToAqi(35.5), 101);
+  });
+
+  it('pm25 of 55.4', () => {
+    assert.equal(convertPm25ToAqi(55.4), 150);
+  });
+
+  it('pm25 of 55.5', () => {
+    assert.equal(convertPm25ToAqi(55.5), 151);
+  });
+
+  it('pm25 of 150.4', () => {
+    assert.equal(convertPm25ToAqi(150.4), 200);
+  });
+
+  it('pm25 of 150.5', () => {
+    assert.equal(convertPm25ToAqi(150.5), 201);
+  });
+
+  it('pm25 of 250.4', () => {
+    assert.equal(convertPm25ToAqi(250.4), 300);
+  });
+
+  it('pm25 of 250.5', () => {
+    assert.equal(convertPm25ToAqi(250.5), 301);
+  });
+
+  it('pm25 of 350.4', () => {
+    assert.equal(convertPm25ToAqi(350.4), 400);
+  });
+
+  it('pm25 of 350.5', () => {
+    assert.equal(convertPm25ToAqi(350.5), 401);
+  });
+
+  it('pm25 of 500.4', () => {
+    assert.equal(convertPm25ToAqi(500.5), 500);
+  });
+
+  it('pm25 off the charts', () => {
+    assert.equal(convertPm25ToAqi(10000), 500);
+  });
+
+
+
+
+
+
 
 
 
