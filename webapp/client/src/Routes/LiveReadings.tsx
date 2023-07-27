c1f19be5811af32ce1fa39f08a820f61055b0b25
Gregory Melsby <greg@gregexp.com>
2023-07-26 22:16:03 -0400

    shows AQI category

--- webapp/client/src/Routes/LiveReadings.tsx
+++ webapp/client/src/Routes/LiveReadings.tsx
@@ -1,4 +1,6 @@
 import { useState, useEffect } from 'preact/hooks';
+import { categorizeAqi, convertPm25ToAqi } from '../utils/AQIUtils';
+import { ComponentChildren } from 'preact';
 
 
 export default function LiveReadings() {
@@ -7,6 +9,7 @@ export default function LiveReadings() {
 
 
   const [isFetching, setIsFetching] = useState(true);
+  const [currentAqi, setCurrentAqi] = useState(0);
   const [currentSample, setCurrentSample] = useState<Sample | undefined>(undefined);
 
   useEffect(() => {
@@ -21,6 +24,7 @@ export default function LiveReadings() {
         } 
         const result = await response.json();
         setCurrentSample(result);
+        setCurrentAqi(convertPm25ToAqi(result.pm25));
         setIsFetching(false);
       };
 
@@ -45,16 +49,20 @@ export default function LiveReadings() {
       {[['PM 1.0', 'pm1'], ['PM 2.5', 'pm25'], ['Particles > 0.3um / 0.1L', 'particles03']].map(([title, sampleKey]) =>
         <DataBox {...{title}} value={currentSample[(sampleKey as keyof Sample)]} key={title}/>
       )}
+      <DataBox title="Equivalent AQI" value={currentAqi}>
+        <p>{categorizeAqi(currentAqi)}</p>
+      </DataBox>
     </div>
     
   );
 }
 
-const DataBox = ({title, value}: {title: string, value: number | string}) => {
+const DataBox = ({title, value, children}: {title: string, value: number | string, children?: ComponentChildren}) => {
   return (
     <div className="border p-5 w-1/2">
       <h5>{title}</h5>
       <h3>{value}</h3>
+      { children }
     </div>
   );
 };
