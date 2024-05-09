ae75aaf79c101b62dfe59949ecaa76614f36862b
Gregory Melsby <greg@gregexp.com>
2024-05-09 17:37:15 -0400

    updated QualityColorSquare to use IconContext.Provider instead of className

--- webapp/client/src/Components/QualityColorSquare.tsx
+++ webapp/client/src/Components/QualityColorSquare.tsx
@@ -1,20 +1,26 @@
 import { IoIosSquare } from 'react-icons/io';
+import { IconContext } from 'react-icons';
 import { AqiCategory } from '../Utils/AQIUtils';
 
 export default function QualityColorSquare({ currentAqiCategory }: {currentAqiCategory: AqiCategory | undefined}) {
-  return <IoIosSquare className={`text-2xl stroke-black ${
-    currentAqiCategory === AqiCategory.Good ?
-      'text-green-500' :
-      currentAqiCategory === AqiCategory.Moderate ?
-        'text-yellow-300' :
-        currentAqiCategory === AqiCategory.UnhealthyForSensitiveGroups ?
-          'text-orange-500' :
-          currentAqiCategory === AqiCategory.Unhealthy ?
-            'text-red-600' :
-            currentAqiCategory === AqiCategory.VeryUnhealthy ?
-              'text-purple-700' :
-              currentAqiCategory === AqiCategory.Hazardous ?
-                'text-amber-900' :
-                'text-black'
-  }`} />;
+  return (
+    <IconContext.Provider value={{
+      className: `text-2xl stroke-black ${
+        currentAqiCategory === AqiCategory.Good ?
+          'text-green-500' :
+          currentAqiCategory === AqiCategory.Moderate ?
+            'text-yellow-300' :
+            currentAqiCategory === AqiCategory.UnhealthyForSensitiveGroups ?
+              'text-orange-500' :
+              currentAqiCategory === AqiCategory.Unhealthy ?
+                'text-red-600' :
+                currentAqiCategory === AqiCategory.VeryUnhealthy ?
+                  'text-purple-700' :
+                  currentAqiCategory === AqiCategory.Hazardous ?
+                    'text-amber-900' :
+                    'text-black'
+      }` }}>
+      <IoIosSquare />
+    </IconContext.Provider>
+  );
 }
